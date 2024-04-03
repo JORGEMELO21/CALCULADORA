467cb3c2c9e42b5c281dd01e5f518f31c9739e91
JORGEMELO21 <142284641+JORGEMELO21@users.noreply.github.com>
2024-04-03 01:19:30 -0300

    Add files via upload
    finalização do projeto.

---
+++ CALCULADORA/somascript.js
@@ -0,0 +1,17 @@
+function somar() {
+    const numero1 = document.getElementById('numero1').value;
+    const numero2 = document.getElementById('numero2').value;
+
+    if (!numero1 || !numero2) {
+        alert('Digite os dois números.');
+        return;
+    }
+
+    const num1 = parseInt(numero1, 10);
+    const num2 = parseInt(numero2, 10);
+
+    const resultado = num1 + num2;
+
+    const elementoResultado = document.getElementById('resultado');
+    elementoResultado.value = resultado;
+}
